9211712cf494587cb6c4c1e5f50b73dee0c085c0
AnnaHey <atekton99@gmail.com>
2017-09-07 13:52:39 -0500

    Add gradle

---
+++ src/main/java/com/practice/medsorder/MedsOrderApplication.java
@@ -0,0 +1,12 @@
+package com.practice.medsorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedsOrderApplication.class, args);
+	}
+}
